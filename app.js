ca696aeddf5ba767154525623d5fa64c9b642173
matt <norrism7@msu.edu>
2023-05-02 04:21:23 -0400

    about page, style change, nav bar, carousel

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const trackUrls = [
   'audio/ImYours.m4a'
 ];
 
-const playlistItems = document.querySelectorAll('.list-group-item');
+const carouselItems = document.querySelectorAll('.carousel-item');
 const trackName = document.querySelector('.player-info h2');
 const artistName = document.querySelector('.player-info h3');
 const albumCover = document.querySelector('.album-cover img');
 let player = {};
-
-const playlistArray = Array.from(playlistItems);
 
 function updateBackgroundColor() {
   const colorThief = new ColorThief();
@@ -20,16 +18,6 @@ function updateBackgroundColor() {
     document.body.style.backgroundColor = rgbColor;
   });
 }
-
-function updateMetadata(index) {
-  const item = playlistArray[index];
- 
-  trackName.innerHTML = item.innerHTML; // Update the track name in the metadata
-  artistName.innerHTML = item.dataset.artist;
-  albumCover.src = item.dataset.cover;
-  updateBackgroundColor(); 
-}
-
 
 function initPlayer(src) {
   if (player && typeof player.unload === 'function') {
@@ -46,28 +34,10 @@ function initPlayer(src) {
 
 initPlayer('audio/all.m4a');
 
-const colorThief = new ColorThief();
 
-const color = colorThief.getColor(albumCover);
-const rgbColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
-
-document.body.style.backgroundColor = rgbColor;
-playlistItems.forEach((item, index) => {
-  item.addEventListener('click', () => {
-    initPlayer(trackUrls[index]); // Initialize the player with the new track URL
-    player.play(); // Play the new track
-    document.getElementById('play-pause')// Update the play/pause button text
-    trackName.innerHTML = item.innerHTML; // Update the track name in the metadata
-    artistName.innerHTML = item.dataset.artist;
-    albumCover.src = item.dataset.cover;
-  });
-});
 
 const playPauseBtn = document.getElementById('play-pause');
-
 const nextBtn = document.getElementById('next');
-const prevBtn = document.getElementById('prev');
-const shuffleBtn = document.getElementById('shuffle');
 const volumeSlider = document.getElementById('volume-slider');
 
 playPauseBtn.addEventListener('click', () => {
@@ -80,28 +50,59 @@ playPauseBtn.addEventListener('click', () => {
   }
 });
 
-
-
-nextBtn.addEventListener('click', () => {
-  currentTrack++;
-  if (currentTrack >= trackUrls.length) currentTrack = 0;
-  initPlayer(trackUrls[currentTrack]);
-  player.play();
-  updateMetadata(currentTrack);
-});
-
-
-prevBtn.addEventListener('click', () => {
-  currentTrack--;
-  if (currentTrack < 0) currentTrack = trackUrls.length - 1;
-  initPlayer(trackUrls[currentTrack]);
-  player.play();
-  updateMetadata(currentTrack);
-});
-
 let currentTrack = 0;
+
+
 
 volumeSlider.addEventListener('input', () => {
   player.volume(volumeSlider.value / 100);
 });
+
+const prevButton = document.querySelector('.carousel-control.prev');
+const nextButton = document.querySelector('.carousel-control.next');
+const playerInfo = document.querySelector('.player-info');
+let activeIndex = 0;
+
+function updateMetadata() {
+  const activeItem = carouselItems[activeIndex];
+  const title = activeItem.querySelector('h2').innerText;
+  const artist = activeItem.querySelector('h3').innerText;
+  const source = activeItem.querySelector('h4').innerText;
+  const cover = activeItem.querySelector('img').src;
+
+  playerInfo.innerHTML = `
+    <h2 class="text-xl font-semibold mb-1">${title}</h2>
+    <h3 class="text-md text-gray-700 mb-1">${artist}</h3>
+    <h4 class="text-sm text-gray-500">${source}</h4>
+  `;
+  document.querySelector('.album-cover img').src = cover;
+  updateBackgroundColor();
+}
+
+function changeCarouselItem(offset){
+carouselItems[activeIndex].classList.remove('active');
+activeIndex = (activeIndex + offset + carouselItems.length) % carouselItems.length;
+carouselItems[activeIndex].classList.add('active');
+updateMetadata();
+}
+
+prevButton.addEventListener('click', () => {
+changeCarouselItem(-1);
+updateTrack();
+});
+
+nextButton.addEventListener('click', () => {
+changeCarouselItem(1);
+updateTrack();
+});
+
+// Initialize with the first item's metadata
+updateMetadata();
+
+// Update the play/pause button and track when the carousel changes
+function updateTrack() {
+initPlayer(trackUrls[activeIndex]);
+player.play();
+playPauseBtn.innerHTML = '<i class="fas fa-pause text-gray-700 cursor-pointer"></i>';
+}
 
